6dfb16ab86d72e40f618f2e5d1c31876bc9b094f
Alyssa Challender <87546582+alysurr@users.noreply.github.com>
2022-02-06 17:10:10 -0500

    added hamburger menu, typography

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.scss'
 
 export default function Header() {
   return (
     <header>
+      <div className="hamburger-nav">
+    <input id="nav__toggle" type="checkbox" />
+    <label className="nav__btn" for="nav__toggle">
+      <span></span>
+    </label>
+
+    <ul className="nav__box">
+      <li><Link className="nav__item" to="/">Home</Link></li>
+      <li><Link className="nav__item" to="/">About</Link></li>
+      <li><Link className="nav__item" to="/">Projects</Link></li>
+      <li><Link className="nav__item" to="/">Skills</Link></li>
+      <li><Link className="nav__item" to="/">Gallery</Link></li>
+    </ul>
+  </div>
         <nav className='header__nav'>
-            <Link to="/">Home</Link>
-            <Link to="/">About</Link>
-            <Link to="/">Projects</Link>
-            <Link to="/">Skills</Link>
-            <Link to="/">Gallery</Link>
+
         </nav>
     </header>
   );
