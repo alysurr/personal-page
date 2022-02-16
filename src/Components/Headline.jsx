5a77920140b4084691da39a03dc979229b745181
Alyssa Challender <87546582+alysurr@users.noreply.github.com>
2022-02-16 10:34:09 -0500

    added some gallery features

--- src/Components/Headline.jsx
+++ src/Components/Headline.jsx
@@ -1,12 +1,11 @@
 import './Headline.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Scene from './Scene'
 
 export default function Headline() {
 
   return <><section className="hero content">
-  <div className="canvas"><Scene /></div>
+  
     <div className="hero__container">
       <h1 className="hero__h1">Alyssa Challender,</h1>
       <h1 className="hero__h1">Junior React Developer</h1>
